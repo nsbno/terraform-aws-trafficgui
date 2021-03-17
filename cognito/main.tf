f7a539a58e10b1e848caef4fd42553afe3f4854f
mariehaga <marie.haga@vy.no>
2021-03-17 11:31:17 +0100

    Changed access token validity to 5 min for testing purposes

--- cognito/main.tf
+++ cognito/main.tf
@@ -19,7 +19,7 @@ locals {
 # upload delegated cognito config to S3 bucket.
 # this will trigger the delegated cognito terraform pipeline and and apply the config.
 resource "aws_s3_bucket_object" "delegated-cognito-config" {
-  count      = var.create_resource_server ? 1 : 0
+  count  = var.create_resource_server ? 1 : 0
   bucket = var.cognito_central_bucket
   key    = "${length(var.cognito_central_override_env) > 0 ? var.cognito_central_override_env : var.environment}/${local.current_account_id}/${var.name_prefix}-${var.application_name}.json"
   acl    = "bucket-owner-full-control"
@@ -34,7 +34,9 @@ resource "aws_s3_bucket_object" "delegated-cognito-config" {
       allowed_oauth_scopes                 = var.app_oauth_scopes
       allowed_oauth_flows_user_pool_client = true
       supported_identity_providers         = var.supported_identity_providers
-      callback_urls                         = var.callback_urls
+      callback_urls                        = var.callback_urls
+      //for testing purposes
+      access_token_validity = 5
     }
   })
   content_type = "application/json"
@@ -51,7 +53,7 @@ resource "aws_s3_bucket_object" "delegated-cognito-config" {
 # The sleep wait will only occur when the dependent S3 file is updated
 # and during normal operation without changes it will not pause here.
 resource "time_sleep" "wait_for_credentials" {
-  count = var.create_app_client ? 1 : 0
+  count           = var.create_app_client ? 1 : 0
   create_duration = "300s"
 
   triggers = {
@@ -62,20 +64,20 @@ resource "time_sleep" "wait_for_credentials" {
 # The client credentials that are stored in Central Cognito.
 data "aws_secretsmanager_secret_version" "microservice_client_credentials" {
   depends_on = [aws_s3_bucket_object.delegated-cognito-config[0], time_sleep.wait_for_credentials[0]]
-  count = var.create_app_client ? 1 : 0
+  count      = var.create_app_client ? 1 : 0
   secret_id  = "arn:aws:secretsmanager:eu-west-1:${var.cognito_central_account_id}:secret:${local.current_account_id}-${var.name_prefix}-${var.application_name}-id"
 }
 
 # Store client credentials from Central Cognito in SSM so that the application can read it.
 resource "aws_ssm_parameter" "central_client_id" {
-  name      =  "/${var.name_prefix}/config/${var.application_name}/cognito.clientId"
+  name      = "/${var.name_prefix}/config/${var.application_name}/cognito.clientId"
   type      = "SecureString"
   value     = jsondecode(data.aws_secretsmanager_secret_version.microservice_client_credentials[0].secret_string)["client_id"]
   overwrite = true
 
   # store the hash as a tag to establish a dependency to the wait_for_credentials resource
-  tags      = merge(var.tags, {
-    config_hash: time_sleep.wait_for_credentials[0].triggers.config_hash
+  tags = merge(var.tags, {
+    config_hash : time_sleep.wait_for_credentials[0].triggers.config_hash
   })
 }
 
