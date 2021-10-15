9d852b678c9a27a343de5fa8d583fecb4b5674ea
Ole Peder Lømo <ole.peder.lomo@vy.no>
2021-10-15 12:02:54 +0200

    Created cognito module for backend applications

---
+++ cognito-backend-applications/main.tf
@@ -0,0 +1,79 @@
+data "aws_region" "current" {}
+
+data "aws_caller_identity" "current" {}
+
+locals {
+  current_account_id = data.aws_caller_identity.current.account_id
+  current_region     = data.aws_region.current.name
+}
+
+###########################################################
+# Create Resource Server and App Client in the central Cognito
+#
+# We need to Create all the oauth configuration in the central
+# cognito and migrate our services over to use it so that
+# the OAuth tokens is valid between different team accounts,
+# For example traffic-control, traffic-gui and info.
+###########################################################
+
+# upload delegated cognito config to S3 bucket.
+# this will trigger the delegated cognito terraform pipeline and and apply the config.
+resource "aws_s3_bucket_object" "delegated-cognito-config" {
+  count  = var.create_resource_server ? 1 : 0
+  bucket = var.cognito_central_bucket
+  key    = "${length(var.cognito_central_override_env) > 0 ? var.cognito_central_override_env : var.environment}/${local.current_account_id}/${var.name_prefix}-${var.application_name}.json"
+  acl    = "bucket-owner-full-control"
+
+  content = jsonencode({
+    # Configure a user pool client
+    user_pool_client = {
+      name_prefix     = "${var.name_prefix}-${var.application_name}"
+      generate_secret = false
+
+      allowed_oauth_flows                  = ["client_credentials"]
+      allowed_oauth_scopes                 = var.app_oauth_scopes
+      allowed_oauth_flows_user_pool_client = true
+    }
+  })
+  content_type = "application/json"
+}
+
+
+##
+# Read Credentials from Secrets Manager and set in microservice SSM config.
+# Store the md5 of the cognito config so that a change in md5/config
+# Will trigger a new update on dependent resources.
+#
+# Using workaround using time_sleep for async pipeline in cognito to complete
+# configuration of resource server and application client in delegated cognito.
+# The sleep wait will only occur when the dependent S3 file is updated
+# and during normal operation without changes it will not pause here.
+resource "time_sleep" "wait_for_credentials" {
+  count           = var.create_app_client ? 1 : 0
+  create_duration = "300s"
+
+  triggers = {
+    config_hash = sha1(aws_s3_bucket_object.delegated-cognito-config[0].content)
+  }
+}
+
+# The client credentials that are stored in Central Cognito.
+data "aws_secretsmanager_secret_version" "microservice_client_credentials" {
+  depends_on = [aws_s3_bucket_object.delegated-cognito-config[0], time_sleep.wait_for_credentials[0]]
+  count      = var.create_app_client ? 1 : 0
+  secret_id  = "arn:aws:secretsmanager:eu-west-1:${var.cognito_central_account_id}:secret:${local.current_account_id}-${var.name_prefix}-${var.application_name}-id"
+}
+
+# Store client credentials from Central Cognito in SSM so that the application can read it.
+resource "aws_ssm_parameter" "central_client_id" {
+  name      = "/${var.name_prefix}/config/${var.application_name}/cognito.clientId"
+  type      = "SecureString"
+  value     = jsondecode(data.aws_secretsmanager_secret_version.microservice_client_credentials[0].secret_string)["client_id"]
+  overwrite = true
+
+  # store the hash as a tag to establish a dependency to the wait_for_credentials resource
+  tags = merge(var.tags, {
+    config_hash : time_sleep.wait_for_credentials[0].triggers.config_hash
+  })
+}
+
---
+++ cognito-backend-applications/variables.tf
@@ -0,0 +1,49 @@
+variable "name_prefix" {
+  description = "A prefix used for naming resources."
+}
+
+variable "application_name" {
+  description = "Name of the application"
+}
+
+variable "environment" {
+  description = "Name of the environment, Ex. dev, test ,stage, prod."
+  type        = string
+}
+
+variable "app_oauth_scopes" {
+  description = "List of allowed OAuth scopes (phone, email, openid, profile, and aws.cognito.signin.user.admin)."
+  type        = list(string)
+}
+
+variable "cognito_central_bucket" {
+  description = "(Optional) Configure where to upload delegated cognito config. Default is vydev-delegated-cognito-staging."
+  type        = string
+  default     = "vydev-delegated-cognito-staging"
+}
+
+variable "cognito_central_override_env" {
+  description = " Override which env to upload to for delegated cognito, default is the \"environment\"-variable."
+  type        = string
+  default     = ""
+}
+
+variable "cognito_central_account_id" {
+  description = "Set cognito account id from where to read the Client ID and Client Secret from."
+  type        = string
+}
+
+variable "tags" {
+  type = map(string)
+}
+
+variable "create_resource_server" {
+  description = "Create resource server in Cognito for microservice."
+  default     = false
+  type        = bool
+}
+
+variable "create_app_client" {
+  description = "Create application client in Cognito for microservice."
+  type        = bool
+}
